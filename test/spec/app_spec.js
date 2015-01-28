7b0dcf5ddbc75a67234fb9ef60971e941afa784a
fishermanswharff <fishermanswharff@github.com>
2015-01-27 23:39:01 -0500

    tests

--- test/spec/app_spec.js
+++ test/spec/app_spec.js
@@ -14,3 +14,185 @@ describe("the application", function() {
   });
 });
 
+/*
+Suites: describe Your Tests
+——————————————————————————————————————————————————————————————————————
+A test suite begins with a call to the global Jasmine
+function describe with two parameters: a string and a function.
+The string is a name or title for a spec suite – usually what
+is being tested. The function is a block of code that implements the suite.
+
+*/
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+
+describe("A suite is just a function", function() {
+  var a;
+
+  it("and so is a spec", function() {
+    a = true;
+
+    expect(a).toBe(true);
+  });
+});
+
+describe("The 'toBe' matcher compares with ===", function() {
+  it("and has a positive case", function() {
+    expect(true).toBe(true);
+  });
+   it("and can have a negative case", function() {
+    expect(false).not.toBe(true);
+  });
+});
+
+describe("Included matchers:", function() {
+  it("The 'toBe' matcher compares with ===", function() {
+    var a = 12;
+    var b = a;
+
+    expect(a).toBe(b);
+    expect(a).not.toBe(null);
+  });
+
+  describe("The 'toEqual' matcher", function() {
+    it("works for simple literals and variables", function() {
+      var a = 12;
+      expect(a).toEqual(12);
+    });
+
+    it("should work for objects", function() {
+      var foo = {
+        a: 12,
+        b: 34
+      };
+      var bar = {
+        a: 12,
+        b: 34
+      };
+      expect(foo).toEqual(bar);
+    });
+  });
+
+  it("The 'toMatch' matcher is for regular expressions", function() {
+    var message = "foo bar baz";
+
+    expect(message).toMatch(/bar/);
+    expect(message).toMatch("bar");
+    expect(message).not.toMatch(/quux/);
+  });
+
+  it("The 'toBeDefined' matcher compares against `undefined`", function() {
+    var a = {
+      foo: "foo"
+    };
+
+    expect(a.foo).toBeDefined();
+    expect(a.bar).not.toBeDefined();
+  });
+
+  it("The `toBeUndefined` matcher compares against `undefined`", function() {
+    var a = {
+      foo: "foo"
+    };
+
+    expect(a.foo).not.toBeUndefined();
+    expect(a.bar).toBeUndefined();
+  });
+
+  it("The 'toBeNull' matcher compares against null", function() {
+    var a = null;
+    var foo = "foo";
+
+    expect(null).toBeNull();
+    expect(a).toBeNull();
+    expect(foo).not.toBeNull();
+  });
+
+  it("The 'toBeTruthy' matcher is for boolean casting testing", function() {
+    var a, foo = "foo";
+
+    expect(foo).toBeTruthy();
+    expect(a).not.toBeTruthy();
+  });
+
+  it("The 'toBeFalsy' matcher is for boolean casting testing", function() {
+    var a, foo = "foo";
+
+    expect(a).toBeFalsy();
+    expect(foo).not.toBeFalsy();
+  });
+
+  it("The 'toContain' matcher is for finding an item in an Array", function() {
+    var a = ["foo", "bar", "baz"];
+
+    expect(a).toContain("bar");
+    expect(a).not.toContain("quux");
+  });
+
+  it("The 'toBeLessThan' matcher is for mathematical comparisons", function() {
+    var pi = 3.1415926,
+      e = 2.78;
+
+    expect(e).toBeLessThan(pi);
+    expect(pi).not.toBeLessThan(e);
+  });
+
+  it("The 'toBeGreaterThan' matcher is for mathematical comparisons", function() {
+    var pi = 3.1415926,
+      e = 2.78;
+
+    expect(pi).toBeGreaterThan(e);
+    expect(e).not.toBeGreaterThan(pi);
+  });
+
+  it("The 'toBeCloseTo' matcher is for precision math comparison", function() {
+    var pi = 3.1415926,
+      e = 2.78;
+
+    expect(pi).not.toBeCloseTo(e, 2);
+    expect(pi).toBeCloseTo(e, 0);
+  });
+
+  it("The 'toThrow' matcher is for testing if a function throws an exception", function() {
+    var foo = function() {
+      return 1 + 2;
+    };
+    var bar = function() {
+      return a + 1;
+    };
+
+    expect(foo).not.toThrow();
+    expect(bar).toThrow();
+  });
+});
+
+
+/*
+Grouping Related Specs with describe
+——————————————————————————————————————————————————————————————————————
+The describe function is for grouping related specs.
+The string parameter is for naming the collection of specs,
+and will be concatenated with specs to make a spec’s full name.
+This aids in finding specs in a large suite. If you name
+them well, your specs read as full sentences in traditional BDD style.
+*/
+describe("A spec", function() {
+  it("is just a function, so it can contain any code", function() {
+    var foo = 0;
+    foo += 1;
+
+    expect(foo).toEqual(1);
+  });
+
+  it("can have more than one expectation", function() {
+    var foo = 0;
+    foo += 1;
+
+    expect(foo).toEqual(1);
+    expect(true).toEqual(true);
+  });
+});
