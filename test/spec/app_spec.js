001668f68a82ccbf865958f91c9ca5d36e929865
fishermanswharff <fishermanswharff@github.com>
2015-01-28 00:00:53 -0500

    spec

--- test/spec/app_spec.js
+++ test/spec/app_spec.js
@@ -30,6 +30,19 @@ describe("A suite", function() {
   });
 });
 
+
+/*
+Specs
+——————————————————————————————————————————————————————————————————————
+Specs are defined by calling the global Jasmine function it, which,
+like describe takes a string and a function. The string is the title
+of the spec and the function is the spec, or test. A spec contains
+one or more expectations that test the state of the code. An
+expectation in Jasmine is an assertion that is either true or false.
+A spec with all true expectations is a passing spec. A spec with one
+or more false expectations is a failing spec.
+*/
+
 describe("A suite is just a function", function() {
   var a;
 
@@ -40,6 +53,22 @@ describe("A suite is just a function", function() {
   });
 });
 
+/*
+It’s Just Functions
+——————————————————————————————————————————————————————————————————————
+Since describe and it blocks are functions, they can contain any 
+executable code necessary to implement the test. JavaScript scoping 
+rules apply, so variables declared in a describe are available to 
+any it block inside the suite.
+*/
+
+/*
+Expectations
+——————————————————————————————————————————————————————————————————————
+Expectations are built with the function expect which takes a value, 
+called the actual. It is chained with a Matcher function, which takes the expected value.
+*/
+
 describe("The 'toBe' matcher compares with ===", function() {
   it("and has a positive case", function() {
     expect(true).toBe(true);
@@ -49,6 +78,18 @@ describe("The 'toBe' matcher compares with ===", function() {
   });
 });
 
+/*
+Matchers
+——————————————————————————————————————————————————————————————————————
+Each matcher implements a boolean comparison between the actual
+value and the expected value. It is responsible for reporting to
+Jasmine if the expectation is true or false. Jasmine will then
+pass or fail the spec.
+
+Any matcher can evaluate to a negative assertion by chaining
+the call to expect with a not before calling the matcher.
+
+*/
 describe("Included matchers:", function() {
   it("The 'toBe' matcher compares with ===", function() {
     var a = 12;
@@ -58,6 +99,16 @@ describe("Included matchers:", function() {
     expect(a).not.toBe(null);
   });
 
+
+  /*
+  Included Matchers
+  ——————————————————————————————————————————————————————————————————————
+  Jasmine has a rich set of matchers included.
+  Each is used here – all expectations and specs pass.
+  There is also the ability to write custom matchers
+  for when a project’s domain calls for specific
+  assertions that are not included below.
+  */
   describe("The 'toEqual' matcher", function() {
     it("works for simple literals and variables", function() {
       var a = 12;
@@ -196,3 +247,193 @@ describe("A spec", function() {
     expect(true).toEqual(true);
   });
 });
+
+/*
+Setup and Teardown
+——————————————————————————————————————————————————————————————————————
+To help a test suite DRY up any duplicated setup and teardown code,
+Jasmine provides the global beforeEach, afterEach, beforeAll, and
+afterAll functions.
+
+As the name implies, the beforeEach function is called once before
+each spec in the describe is run, and the afterEach function is
+called once after each spec.
+
+Here is the same set of specs written a little differently. The
+variable under test is defined at the top-level scope — the describe
+block — and initialization code is moved into a beforeEach function.
+The afterEach function resets the variable before continuing.
+*/
+
+describe("A spec using beforeEach and afterEach", function() {
+  var foo = 0;
+
+  beforeEach(function() {
+    foo += 1;
+  });
+
+  afterEach(function() {
+    foo = 0;
+  });
+
+  it("is just a function, so it can contain any code", function() {
+    expect(foo).toEqual(1);
+  });
+
+  it("can have more than one expectation", function() {
+    expect(foo).toEqual(1);
+    expect(true).toEqual(true);
+  });
+});
+
+
+/*
+The beforeAll function is called only once before all the specs 
+in describe are run, and the afterAll function is called after 
+all specs finish. These functions can be used to speed up test 
+suites with expensive setup and teardown.
+
+However, be careful using beforeAll and afterAll! Since they 
+are not reset between specs, it is easy to accidentally leak 
+state between your specs so that they erroneously pass or fail.
+*/
+
+describe("A spec using beforeAll and afterAll", function() {
+  var foo;
+
+  beforeAll(function() {
+    foo = 1;
+  });
+
+  afterAll(function() {
+    foo = 0;
+  });
+
+  it("sets the initial value of foo before specs run", function() {
+    expect(foo).toEqual(1);
+    foo += 1;
+  });
+
+  it("does not reset foo between specs", function() {
+    expect(foo).toEqual(2);
+  });
+});
+
+/*
+The this keyword
+——————————————————————————————————————————————————————————————————————
+Another way to share variables between a beforeEach, it, and 
+afterEach is through the this keyword. Each spec’s beforeEach/it/afterEach 
+has the this as the same empty object that is set back to empty for 
+the next spec’s beforeEach/it/afterEach.
+*/
+describe("A spec", function() {
+  beforeEach(function() {
+    this.foo = 0;
+  });
+
+  it("can use the `this` to share state", function() {
+    expect(this.foo).toEqual(0);
+    this.bar = "test pollution?";
+  });
+
+  it("prevents test pollution by having an empty `this` created for the next spec", function() {
+    expect(this.foo).toEqual(0);
+    expect(this.bar).toBe(undefined);
+  });
+});
+
+
+/*
+Nesting describe Blocks
+——————————————————————————————————————————————————————————————————————
+Calls to describe can be nested, with specs defined at any level. 
+This allows a suite to be composed as a tree of functions. Before 
+a spec is executed, Jasmine walks down the tree executing each 
+beforeEach function in order. After the spec is executed, 
+Jasmine walks through the afterEach functions similarly.
+*/
+
+describe("A spec", function() {
+  var foo;
+
+  beforeEach(function() {
+    foo = 0;
+    foo += 1;
+  });
+
+  afterEach(function() {
+    foo = 0;
+  });
+
+  it("is just a function, so it can contain any code", function() {
+    expect(foo).toEqual(1);
+  });
+
+  it("can have more than one expectation", function() {
+    expect(foo).toEqual(1);
+    expect(true).toEqual(true);
+  });
+
+  describe("nested inside a second describe", function() {
+    var bar;
+
+    beforeEach(function() {
+      bar = 1;
+    });
+
+    it("can reference both scopes as needed", function() {
+      expect(foo).toEqual(bar);
+    });
+  });
+});
+
+
+
+/*
+Disabling Suites
+——————————————————————————————————————————————————————————————————————
+Suites and specs can be disabled with the xdescribe and xit functions, 
+respectively. These suites and any specs inside them are skipped 
+when run and thus their results will not appear in the results.
+*/
+
+xdescribe("A spec", function() {
+  var foo;
+
+  beforeEach(function() {
+    foo = 0;
+    foo += 1;
+  });
+
+  it("is just a function, so it can contain any code", function() {
+    expect(foo).toEqual(1);
+  });
+});
+
+
+/*
+Pending Specs
+——————————————————————————————————————————————————————————————————————
+Pending specs do not run, but their names will show up in the results as pending.
+Any spec declared with xit is marked as pending.
+Any spec declared without a function body will also be marked pending in results.
+And if you call the function pending anywhere in the spec body, no matter the expectations, the spec will be marked pending.
+*/
+
+describe("Pending specs", function() {
+  xit("can be declared 'xit'", function() {
+    expect(true).toBe(false);
+  });
+  it("can be declared with 'it' but without a function");
+  it("can be declared by calling 'pending' in the spec body", function() {
+    expect(true).toBe(false);
+    pending();
+  });
+});
+
+
+
+
+
+
