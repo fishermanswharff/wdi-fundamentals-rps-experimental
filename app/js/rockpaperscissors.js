fd9f25a8258d48b44d5ebc92d58b8e8f7e89ab90
MattB-GA-Boston <matt.brendzel@generalassemb.ly>
2015-01-29 15:11:28 -0500

    Deletes the solution code.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -24,28 +24,20 @@ function randomPlay() {
 function getPlayerMove(move) {
     // If a move is given, your expression should evaluate to that move.
     // If move is not specified / is null, your expression should run getInput()
-    return move || getInput();
+    return /* Your Expression */;
 }
 
 function getComputerMove(move) {
     // If a move is given, your expression should evaluate to that move.
     // If move is not specified / is null, your expression should run randomPlay()
-    return move || randomPlay();
+    return /* Your Expression */;
 }
 
 function getWinner(playerMove,computerMove) {
     // This function should either give us back 'player', 'computer', or 'tie'.
     // The rules of the game are that rock beats scissors, scissors beats paper, and paper beats rock.
     // Assume that the only possible input values we can get are 'rock', 'paper', and 'scissors'.
-    if (playerMove == computerMove) {
-        return 'tie';
-    } else if  ((playerMove == 'rock' && computerMove == 'scissors')||
-                (playerMove == 'paper' && computerMove == 'rock') ||
-                (playerMove == 'scissors' && computerMove == 'paper')) {
-        return 'player';
-    } else {
-        return 'computer';
-    }
+
 }
 
 function playToFive() {
@@ -53,22 +45,11 @@ function playToFive() {
     var playerWins = 0;
     var computerWins = 0;
     // This function should continue to play Rock Paper Scissors until either the player or the computer has won five times.
-    while (playerWins < 5 && computerWins < 5) {
-        var playerMove = getPlayerMove('rock');
-        var computerMove = getComputerMove('paper');
-        var winner = getWinner(playerMove,computerMove);
-        if (winner == 'player') {
-            playerWins += 1;
-        } else if (winner == 'computer') {
-            computerWins += 1;
-        }
-        // After each 'round', display some text in the console indicating who played what, who won, and what the current scoreboard looks like.
-        // For example,
-        //  console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
-        //  console.log("The score is currently " + playerWins + " to " + computerWins + "\n");
-        console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
-        console.log("The score is currently " + playerWins + " to " + computerWins + "\n");
-    }
+
+    // After each 'round', display some text in the console indicating who played what, who won, and what the current scoreboard looks like.
+    // For example,
+    //  console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+    //  console.log("The score is currently " + playerWins + " to " + computerWins + "\n");
     return [playerWins, computerWins];
 }
 
